16e1762ac2319cf9a654b272ebda090c245363b5
inishchith <inishchith@gmail.com>
2018-12-12 15:00:07 +0530

    Viewing Changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var channelName = location.search.split('username=')[1]
 if (!channelName) {
-    console.log("Do Not Cheat REDIRECTING... ");
+    alert("Do Not Cheat REDIRECTING... :P")
+    history.go(-1);
 } else {
     console.log("You're Welcome");
     console.log(channelName);
@@ -15,12 +16,29 @@ var pubnub = PUBNUB.init({
 })
 
 function initMap() {
+
+    var peoplePlacesName = [];
     var i, map;
+
+    var markerIcon = {
+        url: './assets/icons/marker.png',
+        scaledSize: new google.maps.Size(40, 40),
+        origin: new google.maps.Point(0, 0), // used if icon is a part of sprite, indicates image position in sprite
+        anchor: new google.maps.Point(20,40) // lets offset the marker image
+      };
+
+      var centroidIcon = {
+        url: './assets/icons/centroid.png',
+        scaledSize: new google.maps.Size(40, 40),
+        origin: new google.maps.Point(0, 0), // used if icon is a part of sprite, indicates image position in sprite
+        anchor: new google.maps.Point(20,40) // lets offset the marker image
+      };
 
     function clearMap() {
         map = new google.maps.Map(
             document.getElementById('map'), {
-                zoom: 9,
+                zoom: 8,
+                mapTypeControl: false,
                 styles: [{
                     "featureType": "administrative.country",
                     "elementType": "geometry",
@@ -51,10 +69,16 @@ function initMap() {
         centroid, centroidMarker, allMarkers = [];
 
     var RADIUS_SPAN = 1500,
-        MAX_PLACES = 5;
+        MAX_PLACES = 3;
 
     // DOM DEFNS ;
-    var add_person = document.getElementById('add_person');
+    var add_people = document.getElementById('add_people');
+    var remove_people = document.getElementById('remove_people');
+    var OpenBar = document.getElementById('Open');
+    var CloseBar = document.getElementById('Close');
+    var ResultBar = document.getElementById('ResultBar');
+
+
     var locationInput = document.getElementById('autocomplete-input');
 
     var autocomplete_one = new google.maps.places.Autocomplete(locationInput);
@@ -111,6 +135,7 @@ function initMap() {
                 labelContent: "YOUR MIDWAY",
                 labelInBackground: false,
                 fillColor: '#000000',
+                icon:centroidIcon
                 // icon: pinImage
             });
             allMarkers.push(centroidMarker);
@@ -120,10 +145,19 @@ function initMap() {
                 infowindow.open(map, centroidMarker);
             });
 
-            map.setZoom(12);
+            map.setZoom(13);
             map.setCenter(new google.maps.LatLng(centroid.lat, centroid.lng));
 
 
+            var circle = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0,
+                fillColor: '#FF0000',
+                fillOpacity: 0.25,
+                map: map,
+                center: new google.maps.LatLng(centroid.lat, centroid.lng),
+                radius: RADIUS_SPAN
+            });
         } else if (n_cords == 1) {
             console.log("ONLY SINGLE COORDINATES ");
             // : SHOW NEARBY THOUGH 
@@ -137,7 +171,7 @@ function initMap() {
 
     // REVERSE GEOCODING
 
-    function reverse_geocoding(point, marker) {
+    function reverse_geocoding(point, markerR) {
 
         geocoder.geocode({
             'location': point
@@ -145,22 +179,22 @@ function initMap() {
 
             if (status === 'OK') {
                 if (results[0]) {
-                    google.maps.event.addListener(marker, 'click', function () {
+                    google.maps.event.addListener(markerR, 'click', function () {
 
                         //TIME PASS
                         function toggleBounce() {
-                            if (marker.getAnimation() != null) {
-                                marker.setAnimation(null);
+                            if (markerR.getAnimation() != null) {
+                                markerR.setAnimation(null);
                             } else {
-                                marker.setAnimation(google.maps.Animation.BOUNCE);
+                                markerR.setAnimation(google.maps.Animation.BOUNCE);
                             }
                         }
 
                         toggleBounce();
                         console.log(results[0] + "INSIDE REVERSE GEOCODING ");
                         // infowindow.setContent("<b>" + results[0].name + "</b> <hr>" + results[0].vicinity);
-                        infowindow.setContent("<b>" + results[0].formatted_address + "<hr>" + "<a href='http://www.google.com/maps/place/" + point.lat + "," + point.lng + "' target='_blank'> Google Map > </a></b>");
-                        infowindow.open(map, marker);
+                        infowindow.setContent("<b>" + results[0].formatted_address + "<hr>" + "<a href='http://www.google.com/maps/place/" + point.lat + "," + point.lng + "' target='_blank'> Get Direction > </a></b>");
+                        infowindow.open(map, markerR);
                         setTimeout(toggleBounce, 1500);
                     });
 
@@ -185,6 +219,7 @@ function initMap() {
             var cord_marker = new google.maps.Marker({
                 position: start,
                 map: map,
+                icon:markerIcon,
                 animation: google.maps.Animation.DROP,
             });
 
@@ -246,40 +281,42 @@ function initMap() {
             ['park'],
             ['bar']
         ];
+
         var icon;
+
+        function createMarker(place) {
+            var placeLoc = place.geometry.location;
+
+            icon = {
+                url: place.icon, // url
+                scaledSize: new google.maps.Size(30, 30), // scaled size
+                // origin: new google.maps.Point(0,0), // origin
+                // anchor: new google.maps.Point(0, 0) // anchor
+            };
+
+            var marker = new google.maps.Marker({
+                position: placeLoc,
+                map: map,
+                icon: icon,
+                animation: google.maps.Animation.DROP,
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent("<b>" + place.name + "</b> <hr>" + place.vicinity + "<hr>" + "<a href='http://www.google.com/maps/place/" + placeLoc.lat() + "," + placeLoc.lng() + "' target='_blank'> Get Direction > </a></b>");
+                infowindow.open(map, marker);
+            });
+
+        }
 
         function callback(results, status) {
             if (status == google.maps.places.PlacesServiceStatus.OK) {
                 for (i = 0; i < Math.min(MAX_PLACES, results.length); i++) {
                     // console.log(results[i]);
-                    var place = results[i];
-
-                    var placeLoc = place.geometry.location;
-
-                    icon = {
-                        url: place.icon, // url
-                        scaledSize: new google.maps.Size(30, 30), // scaled size
-                        // origin: new google.maps.Point(0,0), // origin
-                        // anchor: new google.maps.Point(0, 0) // anchor
-                    };
-
-                    var marker = new google.maps.Marker({
-                        position: placeLoc,
-                        map: map,
-                        icon: icon,
-                        animation: google.maps.Animation.DROP,
-                    });
-                    allMarkers.push(marker);
-                    // console.log(place.name);
-
-                    google.maps.event.addListener(marker, 'click', function () {
-                        infowindow.setContent("<b>" + place.name + "</b> <hr>" + place.vicinity);
-                        infowindow.open(map, marker);
-                    });
+                    createMarker(results[i]);
                     // reverse_geocoding(cords[i], cord_marker);
                 }
             }
         }
+
         for (i = 0; i < types.length; i++) {
 
             service.nearbySearch({
@@ -305,11 +342,14 @@ function initMap() {
 
     function addPeople() {
         if (objLocation.formatted_address) {
+            console.log(objLocation.formatted_address,objLocation);
+            
             cord = {
                 lat: objLocation.geometry.location.lat(),
                 lng: objLocation.geometry.location.lng(),
             }
             console.log(cord);
+            peoplePlacesName.push(objLocation.formatted_address);
             groupCoordinates.push(cord);
             n_cords += 1;
             // console.log(objLocation.formatted_address+" HEY");
@@ -323,7 +363,7 @@ function initMap() {
                     animation: google.maps.Animation.DROP,
                 });
 
-                reverse_geocoding(cord, newMarker);
+                // reverse_geocoding(cord, newMarker);
 
                 allMarkers.push(newMarker);
             } else {
@@ -338,29 +378,63 @@ function initMap() {
 
 
     // ADDED PERSON 
-    add_person.onclick = function () {
+    add_people.onclick = function () {
         addPeople();
-        // publish Data
+
+        // PUBLISH DATA
         publish({
             n_points: n_cords,
-            points: groupCoordinates
+            points: groupCoordinates,
+            placeNames:peoplePlacesName
         });
     }
 
-    // remove_person.onclick = function(){
-    //     // publish Data
-    // }
+    // REMOVE ALL 
+    remove_people.onclick = function () {
+        n_cords = 0;
+        groupCoordinates = [];
+        peoplePlacesName = []
+
+        // PUBLISH DATA
+        publish({
+            n_points: n_cords,
+            points: groupCoordinates,
+            placeNames:peoplePlacesName
+        });
+        clearMap();
+        location.reload();
+    }
+
+    // CLOSE BAR
+    CloseBar.onclick = function(){
+        ResultBar.style.display = "none";
+        CloseBar.style.display = "none";
+        OpenBar.style.display = "";
+    }
+    //OPEN BAR
+    OpenBar.onclick = function(){
+        ResultBar.style.display = "";
+        CloseBar.style.display = "";
+        OpenBar.style.display = "none";
+    }
+
 
     function showPeople() {
         var people = document.getElementById("online_people"),
             li;
         people.innerHTML = '';
-
+        console.log(groupCoordinates);
         for (i = 0; i < n_cords; i++) {
             li = document.createElement("li");
-            li.innerHTML = groupCoordinates[i].lat + " " + groupCoordinates[i].lng;
+            var name = peoplePlacesName[i]
+            var len = peoplePlacesName[i].length ;
+            if(len > 30){
+                name = name.substring(0,30);
+                name += " ..";
+            }
+            
+            li.innerHTML = "<p> <i class='material-icons green-text inline-icon'>adjust</i>" + name +"  </p>"
             people.appendChild(li);
-
         }
     }
 
@@ -368,11 +442,13 @@ function initMap() {
     infowindow.open(map, centroidMarker);
 
     function publishedData(data) {
-
-        if (!data) {}
+        if (!data) {
+            console.log("NO DATA")
+        }
         console.log(data);
         n_cords = data.n_points;
         groupCoordinates = data.points;
+        peoplePlacesName = data.placeNames
         updateView();
     }
 
@@ -382,8 +458,9 @@ function initMap() {
         channel: channelName,
         callback: publishedData,
         presence: function (m) {
-            if (m.occupancy > 1) {
-                document.getElementById('unit').textContent = 'doodlers';
+           if (m.occupancy ) {
+                // document.getElementById('unit').textContent = 'Viewers';
+                document.getElementById('unit').innerHTML = "<i class='material-icons'>person</i>";
             }
             document.getElementById('occupancy').textContent = m.occupancy;
             var p = document.getElementById('occupancy').parentNode;
@@ -404,7 +481,7 @@ function initMap() {
     if (showHistory) {
         pubnub.history({
             channel: channelName,
-            count: 50,
+            count: 1,
             callback: function (messages) {
                 pubnub.each(messages[0], publishedData);
             }
